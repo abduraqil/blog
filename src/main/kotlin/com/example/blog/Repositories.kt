08c45209a7fe517173a795cf3e4822b91440034a
Abdur Rehman Aqil <aqilabdur@gmail.com>
2026-04-19 16:56:13 -0400

    article and user repositories

--- src/main/kotlin/com/example/blog/Repositories.kt
+++ src/main/kotlin/com/example/blog/Repositories.kt
@@ -4,7 +4,8 @@ import org.springframework.data.repository.CrudRepository
 
 interface ArticleRepository: CrudRepository<Article, Long> {
     fun findBySlug(slug: String): Article?
-    fun findAllByOrderByAddedAtDesc(): Iterable<Article>
+    fun findAllByOrderByAddedAtDesc(): List<Article>
+    //fun findAllByOrderByAddedAtDesc(): Iterable<Article>
 }
 
 interface UserRepository: CrudRepository<User, Long> {
