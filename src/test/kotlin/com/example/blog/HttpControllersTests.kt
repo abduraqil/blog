8de34e6af71c9a36e558048ef2a107b75ad1fc04
Abdur Rehman Aqil <aqilabdur@gmail.com>
2026-04-25 18:37:16 -0400

    Tests: added tests using Mockk instead, still working

--- src/test/kotlin/com/example/blog/HttpControllersTests.kt
+++ src/test/kotlin/com/example/blog/HttpControllersTests.kt
@@ -1,4 +1,29 @@
 package com.example.blog
 
-class HttpControllersTests {
+import com.ninjasquad.springmockk.MockkBean
+import io.mockk.every
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+import org.springframework.data.jdbc.core.mapping.AggregateReference
+import java.util.Optional
+
+@WebMvcTest
+class HttpControllersTests(@Autowired val mockMvc: MockMvc) {
+    @MockkBean
+    lateinit var userRepository: UserRepository
+
+    @MockkBean
+    lateinit var articleRepository: ArticleRepository
+
+    @Test
+    fun `List articles`() {
+        val johnDoe = User("johnDoe", "John", "Doe", id = 1L)
+        val authorRef =
+    }
+
 }
